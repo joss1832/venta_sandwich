4ef4f6e31f5197ea7572deaef32bcc0e1a9460e5
Josue <21387996@uagro.mx>
2025-02-18 10:51:36 -0600

    adicionales

--- js/main.js
+++ js/main.js
@@ -6,24 +6,170 @@ let carne = 200;
 let vegetariano = 100;
 let blanco = 50;
 let negro = 60;
-let sin_Gluten = 75;
+let sinGluten = 75;
+
+let queso=false;
+let tomate=true;
+let lechuga=false;
+let cebolla=true;
+let mayonesa=false;
+let mostaza=true;
+
 
 if (sandwich_Base == 150 && tipo_Pan==50 ){
-    console.log('tu total a pagar es: ' + sandwich_Base+tipo_Pan)
+    let total=sandwich_Base+tipo_Pan
+    console.log('tu total a pagar es: ' + total)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
+
+
+
+
+
 } else if(sandwich_Base==150 && tipo_Pan==60){
-    console.log('tu total a pagar es: ' + sandwich_Base+tipo_Pan)
+    let total=sandwich_Base+tipo_Pan
+    console.log('tu total a pagar es: ' + total)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
+
 } else if(sandwich_Base==150 && tipo_Pan==75){
-    console.log('tu total es de: ' + sandwich_Base+tipo_Pan)
+    let total=sandwich_Base+tipo_Pan
+    console.log('tu total es de: ' + total)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
+
 } else if (sandwich_Base==200 && tipo_Pan==50){
-    console.log('tu total a pagar es: ' + sandwich_Base+ tipo_Pan)
+    let total=sandwich_Base+tipo_Pan
+    console.log('tu total a pagar es: ' + total)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
+
 } else if(sandwich_Base==200 && tipo_Pan==60){
-    console.log('tu toal a pagar es de: ' + sandwich_Base+tipo_Pan)
+    let total=sandwich_Base+tipo_Pan
+    console.log('tu toal a pagar es de: ' + total)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
+
 } else if(sandwich_Base==200 && tipo_Pan==75){
-    console.log('tu total a pagar es de: ' + sandwich_Base+tipo_Pan)
+    let total=sandwich_Base+tipo_Pan
+    console.log('tu total a pagar es de: ' + total)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
+
 }else if (sandwich_Base==100 && tipo_Pan==50){
-    console.log('tu total a pagar es de: ' + sandwich_Base+tipo_Pan)
+    let total=sandwich_Base+tipo_Pan
+    console.log('tu total a pagar es de: ' + total)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
+
 }else if(sandwich_Base==100 && tipo_Pan==60){
-    console.log('tu total a pagar es de: ' + sandwich_Base+ tipo_Pan)
+    let total=sandwich_Base+tipo_Pan
+    console.log('tu total a pagar es de: ' + total)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
 }else{
     console.log('tu total a pagar es de: ' + sandwich_Base+tipo_Pan)
+    if (queso==true){
+        console.log('tu total es de: ' + total+20)
+    }else if(tomate==true){
+        console.log('tu total es de: ' + total+15)
+    }else if (lechuga==true){
+        console.log('tu total es de: ' + total+ 10)
+    }else if (cebolla==true){
+        console.log('tu total es de: ' + total +5)
+    }else if (mayonesa==true){
+        console.log('tu total es de: ' + total + 15)
+    }else{
+        console.log('tu total es de: ' + total + 5)
+    }
 };
+
+
